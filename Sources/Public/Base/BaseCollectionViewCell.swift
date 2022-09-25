a1bdc99132bedf1a128e1c7b9ea68b21aef00dab
Dmitry Kuznetsov <dsk1306@icloud.com>
2022-09-25 16:12:12 +0200

    Updated BaseCollectionViewCell

--- Sources/Public/Base/BaseCollectionViewCell.swift
+++ Sources/Public/Base/BaseCollectionViewCell.swift
@@ -4,41 +4,41 @@ import UIKit
 
 /// Base class for all collection view cells.
 open class BaseCollectionViewCell: UICollectionViewCell {
-
+    
     // MARK: - Properties
-
+    
     public var reusableCancellable: CombineCancellable { reusableCancellableRelay }
-
+    
     private var reusableCancellableRelay = CombineCancellable()
-
+    
     // MARK: - Initialization
-
+    
     public override init(frame: CGRect) {
         super.init(frame: frame)
-
+        
         configureSubviews()
         reusableBind()
     }
-
+    
     @available(*, unavailable)
     required public init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     // MARK: - Base Class
-
+    
     open override func prepareForReuse() {
         super.prepareForReuse()
-
+        
         reusableCancellableRelay = CombineCancellable()
         reusableBind()
     }
-
+    
     // MARK: - Public Methods
-
+    
     /// Configures cell subviews.
     open func configureSubviews() {}
-
+    
     open func reusableBind() {}
-
+    
 }
