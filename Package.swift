6539459895c1a027567a3c35c5431a1f89606d3f
Dmitry Kuznetsov <dsk1306@icloud.com>
2022-09-25 15:46:08 +0200

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@ import PackageDescription
 let package = Package(
     name: "UIKitExtensions",
     platforms: [
-      .iOS(.v15),
-      .macOS(.v12),
-      .macCatalyst(.v15),
-      .tvOS(.v15),
-      .watchOS(.v8)
+        .iOS(.v15),
+        .macOS(.v12),
+        .macCatalyst(.v15),
+        .tvOS(.v15),
+        .watchOS(.v8)
     ],
     products: [
         .library(
@@ -16,9 +16,17 @@ let package = Package(
             targets: ["UIKitExtensions"]
         )
     ],
+    dependencies: [
+        .package(
+            name: "CombineExtensions",
+            url: "https://github.com/dsk1306/combineExtensions",
+            .upToNextMajor(from: "1.0.0")
+        )
+    ],
     targets: [
         .target(
             name: "UIKitExtensions",
+            dependencies: ["CombineExtensions"],
             path: "Sources"
         ),
         .testTarget(
