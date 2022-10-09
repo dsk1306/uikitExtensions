92f49d3f88b2afd89cde08036595e0bf36c8eab4
Dmitry Kuznetsov <dsk1306@icloud.com>
2022-10-09 17:18:07 +0200

    Added BaseViewTests

---
+++ Tests/Base/BaseViewTests.swift
@@ -0,0 +1,43 @@
+import XCTest
+import UIKit
+import UIKitExtensions
+
+final class BaseViewTests: XCTestCase {
+
+    // MARK: - Properties
+
+    private var view = MockView()
+
+    // MARK: - Base Class
+
+    override func setUp() {
+        super.setUp()
+
+        view = .init()
+    }
+
+    // MARK: - Tests
+
+    func test_configureSubviews() {
+        XCTAssertEqual(view.numberOfConfigureSubviewsCalls, 1)
+    }
+
+}
+
+// MARK: - Mock View
+
+private extension BaseViewTests {
+
+    final class MockView: BaseView {
+
+        var numberOfConfigureSubviewsCalls = 0
+
+        override func configureSubviews() {
+            super.configureSubviews()
+
+            numberOfConfigureSubviewsCalls += 1
+        }
+
+    }
+
+}
